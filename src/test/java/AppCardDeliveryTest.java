8eb3a9ba7b3d8178e49aaed3a7e55d4977e52c42
Tatyana Sokolova <sokolovats1270@gmail.com>
2022-10-20 20:50:33 +0300

    Test on part 1

---
+++ src/test/java/AppCardDeliveryTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class AppCardDeliveryTest {
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldGetSuccessAfterSubmissionOfTheForm() {
+        $("[data-test-id='city'] .input__control").setValue("Элиста");
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(generateDate(3));
+        $("[data-test-id='name'] .input__control").setValue("Соколова-Романова Татьяна");
+        $("[data-test-id='phone'] .input__control").setValue("+79031884178");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + generateDate(3)), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+}
